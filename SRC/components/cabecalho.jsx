f74d5259b0a1d6403a2e0850fc9c7a275a8576e6
Robson Astolfo Miranda <astolfomiranda@gmail.com>
2026-06-05 21:19:32 -0300

    Fix: Reset completo do índice de arquivos para corrigir maiúsculas

--- SRC/components/cabecalho.jsx
+++
@@ -1,9 +0,0 @@
-function Cabecalho() {
-  return (
-    <header className="cabecalho">
-      <h1>TADS Tech Store</h1>
-    </header>
-  );
-}
-
-export default Cabecalho;
--- SRC/components/rodape.jsx
+++
@@ -1,9 +0,0 @@
-function Rodape() {
-  return (
-    <footer className="rodape">
-      <p>© 2026 TADS Tech Store</p>
-    </footer>
-  );
-}
-
-export default Rodape;
